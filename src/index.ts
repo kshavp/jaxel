5fc2ec9aa7d79a582345ba896b18a2cdd28b8881
Keshav Priyam <keshavpriyam@gmail.com>
2023-12-31 13:24:42 +0530

    show csv to <table>

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,43 @@
 import express, { Express, Request, Response } from "express";
 import path from 'path';
 import multer from 'multer';
-import { log } from "console";
+import fs from 'fs';
+import csvParser from 'csv-parser';
 
 const app:Express = express();
 const upload = multer({ dest: 'uploads/' });
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
 app.get('/upload', (req: Request, res: Response) => {
-    const indexPath = path.join(__dirname, '../public/index.html');
-    res.sendFile(indexPath);
-    console.log(__dirname);
+    const content:string = "Jaxel!"
+    res.render('index.ejs',{content});
 });
 
 app.post('/upload', upload.single('csvFile'), (req: Request, res: Response) => {
     if (!req.file) {
-        return res.status(400).send('No file uploaded.');
+        const errMsg = 'No file uploaded.';
+        return res.render('table.ejs',{errMsg});
     }
 
     const uploadedFilePath:string = req.file.path;
 
-    res.send(`File uploaded successfully. Path: ${uploadedFilePath}`);
+    const tableData: any[] = [];
+    fs.createReadStream(uploadedFilePath)
+        .pipe(csvParser())
+        .on('data', (data) => {            
+            tableData.push(data);
+        })
+        .on('end', () => {
+            
+            console.log(tableData);   
+            res.render('table.ejs', { tableData });
+            
+            fs.unlinkSync(uploadedFilePath);
+        });
+
 });
 
 app.listen(PORT, () => {
